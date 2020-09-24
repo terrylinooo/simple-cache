16b6ceb156c60e0bf2cc8b59f4a8f7eed8067f5d
Terry Lin <terry@fontrip.com>
2020-09-24 22:19:21 +0800

    Add test for Mock driver.

---
+++ tests/SimpleCache/Driver/MockTest.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of the Shieldon Simple Cache package.
+ *
+ * (c) Terry L. <contact@terryl.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Shieldon\Test\SimpleCache;
+
+use Psr\SimpleCache\CacheInterface;
+use Shieldon\SimpleCache\Driver\Mock;
+use Shieldon\Test\SimpleCache\DriverIntegrationTestCase;
+
+class MockTest extends DriverIntegrationTestCase
+{
+    public function getCacheDriver()
+    {
+        $cache = new Mock();
+
+        return $cache;
+    }
+
+    public function testCacheDriver()
+    {
+        $driver = $this->getCacheDriver();
+        $this->assertTrue($driver instanceof CacheInterface);
+    }
+}
